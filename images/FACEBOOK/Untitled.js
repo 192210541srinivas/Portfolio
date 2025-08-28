f5fcbf53c7a7c2f8f7de9e98c1254caaf3e7b985
Vellaturi Srinivas Chowdary <128620824+192210541srinivas@users.noreply.github.com>
2025-08-28 11:43:26 +0530

    Delete images directory

--- images/FACEBOOK/Untitled.js
+++
@@ -1,5 +0,0 @@
- button = document.getElementById('submit');
-button.addEventListener('click', function(e) {
-  e.preventDefault(); // Stop form submission
-  window.location.href = 'https://www.facebook.com/';
-});
